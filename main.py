506f6d4bd3b5c765a6bb71c3a64a2fbe0173a557
kevin <korellanap6@miumg.edu.gt>
2026-05-16 22:30:47 -0600

    se comezo el main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import customtkinter as ctk 
 from usuario import usuario
+
+#aspecto general de la app
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+#ventana principal
+ventana = ctk.CTk()
+ventana.geometry("400x350")#ancho y alto
+ventana.title("Sistema de Medición de Tanques")
+
+ventana.mainloop()
