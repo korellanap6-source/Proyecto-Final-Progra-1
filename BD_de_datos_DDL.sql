ebc019ebf81ae73e326f16e9583f5cc7f9d255c5
kevin <korellanap6@miumg.edu.gt>
2026-05-30 08:49:29 -0600

    agregando cosas

--- BD_de_datos_DDL.sql
+++ BD_de_datos_DDL.sql
@@ -33,6 +33,9 @@ CREATE TABLE IF NOT EXISTS registros_medidas (
 
 INSERT INTO usuarios (username, password, rol) 
 VALUES 
-('admin', '1234', 'admin'), 
-('Kevin', '1234', 'empleado'),
-('Luis', '1234', 'admin')
+('Kevin', '1234', 'admin'),
+('Maria', '5678', 'empleado'), 
+('Luis', '5678', 'empleado'),
+('Brayan', '1234', 'admin'),
+('Irvin', '1234', 'admin'),
+('Eric', '1234', 'admin');
